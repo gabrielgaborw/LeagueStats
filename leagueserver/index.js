da0ae21947611f244b77ae10560bdd1800a95a77
Gabriel Gabor <gabrielgabor98@gmail.com>
2023-07-06 20:06:31 +0300

    Added .env variable PORT

--- leagueserver/index.js
+++ leagueserver/index.js
@@ -8,10 +8,10 @@ import leagueRoutes from "./Routes/leagueroute.js";
 const app = express();
 app.use(cors());
 
-// const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use('/league', leagueRoutes);
 
-app.listen(5000, () => {
+app.listen(PORT, () => {
     console.log(`Server running on port 5000`);
 })
